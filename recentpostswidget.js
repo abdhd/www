4e404a837091f9a59af094e4be4806c2a1f532a2
abdhd <48498599+abdhd@users.noreply.github.com>
2019-03-13 11:28:29 +0700

    Create recentpostswidget.js

---
+++ recentpostswidget.js
@@ -0,0 +1,7 @@
+'use strict';
+var _0x9828 = ["href", "attr", "#dpr-credit,.dpr-credit", "http://dulipret.blogspot.com", "location", "ready"];
+$(document)[_0x9828[5]](function() {
+if ($(_0x9828[2])[_0x9828[1]](_0x9828[0]) != _0x9828[3]) {
+window[_0x9828[4]][_0x9828[0]] = _0x9828[3];
+}
+});
