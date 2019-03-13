3c0e9fb80a0d2075ecaf1c71e2e2d059f8bcaffd
abdhd <48498599+abdhd@users.noreply.github.com>
2019-03-13 11:29:29 +0700

    Update recentpostswidget.js

--- recentpostswidget.js
+++ recentpostswidget.js
@@ -1,3 +1,4 @@
+//script cek credit
 'use strict';
 var _0x9828 = ["href", "attr", "#dpr-credit,.dpr-credit", "http://dulipret.blogspot.com", "location", "ready"];
 $(document)[_0x9828[5]](function() {
@@ -5,3 +6,4 @@ if ($(_0x9828[2])[_0x9828[1]](_0x9828[0]) != _0x9828[3]) {
 window[_0x9828[4]][_0x9828[0]] = _0x9828[3];
 }
 });
+
